cabdf48fe43b3d79b749e087e2e9df1a100f51fa
Karan Sikka <kssworld93@gmail.com>
2013-03-18 15:58:09 -0700

    signup form in the constant containers

--- v1factory/static/js/dicts/constant-containers.js
+++ v1factory/static/js/dicts/constant-containers.js
@@ -3,7 +3,7 @@ var constantContainers = {
     {
       'type' : 'text-input',
       'content_attribs' : {
-        placeholder : 'Username...'
+        placeholder : 'Username'
       },
       'constant_attribs' : {
         name : 'username'
@@ -18,7 +18,7 @@ var constantContainers = {
     {
       'type' : 'password',
       'content_attribs' : {
-        placeholder : 'Password...'
+        placeholder : 'Password'
       },
       'constant_attribs' : {
         name : 'password',
@@ -41,6 +41,102 @@ var constantContainers = {
       },
       'layout' : {
         top   : 12,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    }
+  ],
+  'signup' : [
+    {
+      'type' : 'text-input',
+      'content_attribs' : {
+        placeholder : 'Username'
+      },
+      'constant_attribs' : {
+        name : 'username'
+      },
+      'layout' : {
+        top   : 0,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    },
+    {
+      'type' : 'password',
+      'content_attribs' : {
+        placeholder : 'Password'
+      },
+      'constant_attribs' : {
+        name : 'password1',
+        type : 'password'
+      },
+      'layout' : {
+        top   : 8,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    },
+    {
+      'type' : 'password',
+      'content_attribs' : {
+        placeholder : 'Confirm password'
+      },
+      'constant_attribs' : {
+        name : 'password2',
+        type : 'password'
+      },
+      'layout' : {
+        top   : 12,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    },
+    {
+      'type' : 'text',
+      'content_attribs' : {
+        placeholder : 'Email'
+      },
+      'constant_attribs' : {
+        name : 'email',
+        type : 'text'
+      },
+      'layout' : {
+        top   : 16,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    },
+    {
+      'type' : 'text',
+      'content_attribs' : {
+        placeholder : 'Display Name'
+      },
+      'constant_attribs' : {
+        name : 'name',
+        type : 'text'
+      },
+      'layout' : {
+        top   : 20,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    },
+    {
+      'type' : 'button',
+      'content_attribs' : {
+        value : 'Log In'
+      },
+      'constant_attribs' : {
+        type : 'submit'
+      },
+      'layout' : {
+        top   : 24,
         left  : 0,
         width : 16,
         height: 4
