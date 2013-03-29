c0ad75f05e8761af355fc0bd7d41a16941f1d20e
Rodney Folz <folz@rodneyfolz.com>
2013-03-29 14:22:44 -0700

    Special-casing for UserProfile compilation/creation.

--- app_builder/django/code_templates/models.py
+++ app_builder/django/code_templates/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 {% for m in models %}
 {{ m.render() }}
 {% endfor %}
+
+# Automatically create a UserProfile when a User is created.
+def create_profile(sender, **kw):
+  user = kw["instance"]
+  if kw["created"]:
+    profile = UserProfile(m_User=user)
+    profile.save()
+post_save.connect(create_profile, sender=User)
