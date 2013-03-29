2e93d3182318bc98860f45a9938d9ea11de98029
Rodney Folz <folz@rodneyfolz.com>
2013-03-29 16:08:58 -0700

    FacebookBackend pre-save connect.

--- app_builder/django/code_templates/models.py
+++ app_builder/django/code_templates/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+from social_auth.signals import pre_update
+from social_auth.backends.facebook import FacebookBackend
+
 {% for m in models %}
 {{ m.render() }}
 {% endfor %}
@@ -13,3 +16,11 @@ def create_profile(sender, **kw):
     profile = UserProfile(m_User=user)
     profile.save()
 post_save.connect(create_profile, sender=User)
+
+def facebook_extra_values(sender, user, response, details, **kwargs):
+    profile = user.get_profile()
+    profile.m_Email = response.get('email')
+    profile.m_First_Name = response.get('first_name')
+    profile.m_Last_Name = response.get('last_name')
+    profile.save()
+pre_update.connect(facebook_extra_values, sender=FacebookBackend)
