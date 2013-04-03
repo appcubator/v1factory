f3def19fe7798c258961cd03f0e851af75b48c08
Ilter Canberk <ilteriscanberk@gmail.com>
2013-04-03 13:15:33 -0700

    Improved the upload form

--- v1factory/static/js/app/views/UploadExcelView.js
+++ v1factory/static/js/app/views/UploadExcelView.js
@@ -3,18 +3,26 @@ define([
   'backboneui'
 ],
 function(Backbone, BackboneUI) {
-  
+
   var UploadExcelView = BackboneUI.ModalView.extend({
     tagName: 'div',
     className: 'upload-excel',
-    
-    initialize: function(entitiyModel) {
+
+    initialize: function(entityModel) {
+      this.entity = entityModel;
+      this.name = entityModel.get('name');
+      this.fields = entityModel.get('fields').toJSON();
       this.render();
     },
-    
+
     render : function(text) {
-      this.el.innerHTML = '<form enctype="multipart/form-data" method="post" action="/app/'+ appId +'/entities/xl/">'+
-                                  '<input type="file" name="file_name" value="Upload"/><input type="submit" value="Upload"></form>';
+      var self = this;
+      this.el.innerHTML = ['<form enctype="multipart/form-data" method="post" class="upload-form" action="/app/'+ appId +'/entities/xl/">'+
+                                  '<input type="file" name="file_name" value="Upload"/>',
+                                  '<input type="hidden" name="entity_name" value="'+ self.name+'">',
+                                  '<input type="hidden" name="fields" value="'+ JSON.stringify(self.fields) +'">',
+                                  '<input type="submit" value="Upload">',
+                           '</form>'].join('\n');
       return this;
     }
   });
