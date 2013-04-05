28214322f27312ed6aa3b39bb97511a276179ef9
v1 Factory <founders@v1factory.com>
2013-04-05 01:02:04 +0000

    FB login button

--- v1factory/static/js/dicts/constant-containers.js
+++ v1factory/static/js/dicts/constant-containers.js
@@ -1,4 +1,21 @@
 var constantContainers = {
+  'facebook' : [
+    {
+      'type' : 'button',
+      'cons_attribs' : {
+        type : 'submit'
+      },
+      'content_attribs' : {
+        value : "Login w/ Facebook"
+      },
+      'layout' : {
+        top   : 0,
+        left  : 0,
+        width : 16,
+        height: 4
+      }
+    }
+  ],
   'twitter' : [
     {
       'type' : 'button',
