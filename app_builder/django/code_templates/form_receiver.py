47d3e79a92bf680733018d6c1adb91bf9fd7f308
v1 Factory <founders@v1factory.com>
2013-04-06 06:06:24 +0000

    spacing

--- app_builder/django/code_templates/form_receiver.py
+++ app_builder/django/code_templates/form_receiver.py
@@ -2,8 +2,8 @@
 def {{ form_receiver.identifier() }}(request):
   """Create an object from data from a POST request."""
   obj = {{ form_receiver.model.identifier() }}()
-  {% for field in form_receiver.included_fields %}
+{% for field in form_receiver.included_fields %}
   obj.{{ field._django_field.identifier() }} = request.POST['{{ field._django_field.identifier() }}']
-  {% endfor %}
+{% endfor %}
   obj.save()
   return ajax_redirect(request, '/')
