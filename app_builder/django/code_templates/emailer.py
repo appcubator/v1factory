1cbdff593209c73c75c4ffe926fa9b604743484e
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-04-08 17:03:23 -0700

    Added a / into the url

--- app_builder/django/code_templates/emailer.py
+++ app_builder/django/code_templates/emailer.py
@@ -10,4 +10,4 @@ def send_email(from_email, to_email, subject, text, html):
     'api_key' : '{{ api_key }}'
   }
 
-  requests.post("http://v1factory.com/sendhostedemail", data=email_info)
+  requests.post("http://v1factory.com/sendhostedemail/", data=email_info)
