9143392cffbf4c22d92309207bf6d1a7a591877a
Karan Sikka <kssworld93@gmail.com>
2013-04-10 12:42:10 -0700

    new app schema

--- app_builder/validator.py
+++ app_builder/validator.py
@@ -1,102 +1,5 @@
 """ Really cool JSON schema validator """
 
-USER_SETTINGS_SCHEMA = { "_type": {}, "_mapping": {
-  "facebook": { "_type": True },
-  "linkedin": { "_type": True },
-  "twitter": { "_type": True },
-  "local": { "_type": True },
-  "fields": {
-    "_type": [],
-    "_each": { "_type": {}, "_mapping": {
-      "name": { "_type" : "" },
-      "required": { "_type": False },
-      "type": { "_type" : "" }
-    }}
-  },
-  "forms": { "_type": [], "_each": { "_type" : {}, "_mapping": {
-    "name": { "_type" : "" },
-    "fields": { "_type" : [], "_each": { "_type" : {}, "_mapping": {
-      "name": { "_type" : "" },
-      "placeholder": { "_type" : "" },
-      "label": { "_type" : "" },
-      "displayType": { "_type" : "" },
-      "type": { "_type" : "" },
-      "options": { "_type" : [], "_each": { "_type" : "" }}
-    }}},
-    "action": { "_type":"" }
-  }}}
-}}
-
-ENTITY_FIELD_SCHEMA = { "_type": [], "_each": { "_type": {}, "_mapping": {
-  "name": { "_type" : "" },
-  "required": { "_type": False },
-  "type": { "_type" : "" }
-}}}
-
-FORM_SCHEMA = { "_type": [], "_each": { "_type" : {}, "_mapping": {
-  "name": { "_type" : "" },
-  "action": { "_type":"" },
-  "fields": { "_type" : [], "_each": { "_type" : {}, "_mapping": {
-    "name": { "_type" : "" },
-    "placeholder": { "_type" : "" },
-    "label": { "_type" : "" },
-    "displayType": { "_type" : "" },
-    "type": { "_type" : "" },
-    "options": { "_type" : [], "_each": { "_type" : "" }}
-  }}},
-}}}
-
-ENTITY_SCHEMA = { "_type": [], "_each": { "_type": {}, "_mapping": {
-  "name": { "_type" : "" },
-  "fields": ENTITY_FIELD_SCHEMA,
-  "forms": FORM_SCHEMA
-}}}
-
-UIELEMENT_SCHEMA = { "_type": [],"_each": { "_type": {}, "_mapping": {
-  "layout": { "_type": {}, "_mapping": {
-    "width": { "_type": 0, "_min": 1, "_max": 64 },
-    "height": { "_type": 0, "_min": 1 },
-    "top": { "_type": 0, "_min": 0 },
-    "left": { "_type": 0, "_min": 0, "_max": 64 }
-  }},
-  "content": { "_type": "" },
-  "container_info": { "_null": True, "_type": {}, "_mapping": {
-    "entity": { "_type": "" },
-    "action": { "_type": "" },
-    "uielements": None
-  }}
-}}}
-
-UIELEMENT_SCHEMA['_each']['_mapping']['container_info']['_mapping']['uielements'] = UIELEMENT_SCHEMA
-
-APP_SCHEMA = { "_type": {}, "_mapping": {
-  "name": {
-    "_type" : "",
-    "_minlength": 2,
-    "_maxlength": 255,
-  },
-  "users": USER_SETTINGS_SCHEMA,
-  "entities": ENTITY_SCHEMA,
-  "pages": { "_type": [], "_each": { "_type": {}, "_mapping": {
-    "name": { "_type" : "" },
-    "url": { "_type": {}, "_mapping": {
-      "urlparts": { "_type": [],
-        "_each": { "_type" : "" }
-      }
-    }},
-    "uielements": UIELEMENT_SCHEMA,
-    "access_level": { "_type" : "" }
-  }}}
-}}
-
-class Xception(Exception):
-
-  def __init__(self, value):
-    self.message = value
-
-  def __str__(self):
-    return str(self.message)
-
 def validate(thing, schema):
   """Return a list of error messages. if there are no errors, the thing successfully validated, no problemo."""
   errors = []
@@ -104,14 +7,14 @@ def validate(thing, schema):
   #  if the thing is null, see if it's allowed to be null, and if it is, let it pass
   if thing is None:
     try: assert('_null' in schema and schema['_null'])
-    except Exception: raise Xception("thing was null but wasn't supposed to be.\n\n\nthing: {}\n\n\nschema:{}".format(repr(thing), schema))
+    except Exception: raise Exception("thing was null but wasn't supposed to be.\n\n\nthing: {}\n\n\nschema:{}".format(repr(thing), schema))
     else: return errors
 
   # make sure the type of the thing matches with the schema
   try:
     assert('_type' in schema)
   except Exception:
-    raise Xception('schema structure doesn\'t begin with _type')
+    raise Exception('schema structure doesn\'t begin with _type')
 
   if type(thing) == type(u""):
     thing = str(thing)
@@ -124,14 +27,14 @@ def validate(thing, schema):
 
   if type(thing) == type([]):
     try: assert('_each' in schema)
-    except Exception: raise Xception('found [] with no _each')
+    except Exception: raise Exception('found [] with no _each')
     for minithing in thing:
       errors.extend(validate(minithing, schema['_each']))
 
   elif type(thing) == type({}):
 
     try: assert('_mapping' in schema)
-    except Exception: raise Xception('found {} with no _mapping')
+    except Exception: raise Exception('found {} with no _mapping')
 
     for key in schema['_mapping']:
       try: assert(key in thing)
@@ -160,7 +63,7 @@ def validate(thing, schema):
     pass
 
   else:
-    raise Xception("type not recognized: {}".format(thing))
+    raise Exception("type not recognized: {}".format(thing))
 
   return errors
 
