a3248fa1d185faa99169bf5275d41c323dd94845
Ilter Canberk <ilteriscanberk@gmail.com>
2013-04-22 02:52:07 -0400

    tutorial YOLO

--- v1factory/static/js/app/tutorial/TutorialView.js
+++ v1factory/static/js/app/tutorial/TutorialView.js
@@ -9,6 +9,8 @@ function(Backbone) {
     className: 'tutorial-view',
     width: 700,
 
+    addr : [0],
+
     events : {
       "click #tutorial-menu-list li" : "clickedMenuItem"
     },
@@ -20,8 +22,12 @@ function(Backbone) {
                       'appendMenuItem',
                       'clickedMenuItem',
                       'chooseSlide',
-                      'selectMenu');
+                      'selectMenu',
+                      'keyhandler');
       this.render();
+      this.chooseSlide(this.addr, true);
+
+      $(window).on('keydown', this.keyhandler);
     },
 
     render : function(img, text) {
@@ -70,29 +76,113 @@ function(Backbone) {
       this.chooseSlide(addr);
     },
 
-    chooseSlide: function(addr) {
+    chooseSlide: function(addr, isNew) {
+      var self = this;
+
       this.addr = addr;
       this.selectMenu();
 
-      var obj = TutorialDirectory[addr[0]];
-      if(addr[1]) {
-        obj = obj.contents[addr[1]];
+      if(!isNew) {
+
+        $(this.modalWindow).animate({
+          marginTop: "800px"
+        }, 240, function() {
+
+          $(this).css({marginTop: '-800px'});
+          var obj = TutorialDirectory[addr[0]];
+          if(addr[1]) {
+            obj = obj.contents[addr[1]];
+          }
+
+          self.showSlide(obj);
+
+        });
+
+        $(this.modalWindow).delay(240).animate({
+          marginTop: "-300px"
+        });
+      }
+      else {
+        var obj = TutorialDirectory[addr[0]];
+        if(addr[1]) {
+          obj = obj.contents[addr[1]];
+        }
+
+        this.showSlide(obj);
       }
 
-      this.showSlide(obj);
+
     },
 
     selectMenu: function (addr) {
       var addrStr = this.addr.join('-');
-      console.log('#'+addrStr);
       this.$el.find('.selected').removeClass('selected');
       $('#'+addrStr).addClass('selected');
     },
 
     showSlide: function(obj) {
-      //this.el.innerHTML = obj.html;
-      // TODO see if this is ok.
       $('.tutorial-content').html(iui.getHTML(obj.view));
+    },
+
+    selectNext: function (obj) {
+      var self = this;
+      var cur = _.last(self.addr);
+
+      if(self.addr.length == 1) {
+        if(TutorialDirectory[cur].contents) {
+          self.addr = [self.addr[0], 0];
+        }
+        else if(TutorialDirectory[cur + 1]) {
+          self.addr = [cur +1];
+        }
+      }
+      else {
+        if(TutorialDirectory[self.addr[0]].contents[self.addr[1] + 1]) {
+          self.addr = [self.addr[0], self.addr[1]+1];
+        }
+        else if(TutorialDirectory[self.addr[0] + 1]) {
+          self.addr = [self.addr[0] + 1];
+        }
+      }
+
+    },
+
+    selectPrevious: function (obj) {
+      var self = this;
+      var cur = _.last(self.addr);
+
+      if(self.addr.length == 1) {
+        if(TutorialDirectory[cur-1].contents) {
+          self.addr = [cur-1, (TutorialDirectory[cur-1].contents.length - 1)];
+        }
+        else if(TutorialDirectory[cur - 1]) {
+          self.addr = [cur - 1];
+        }
+      }
+      else {
+        if(TutorialDirectory[self.addr[0]].contents[self.addr[1] - 1]) {
+          self.addr = [self.addr[0], self.addr[1] - 1];
+        }
+        else if(TutorialDirectory[self.addr[0]]) {
+          self.addr = [self.addr[0]];
+        }
+      }
+    },
+
+    keyhandler: function (e) {
+      var self = this;
+      switch(e.keyCode) {
+        case 39:
+        case 40:
+         self.selectNext();
+         self.chooseSlide(self.addr, false);
+         break;
+        case 37:
+        case 38:
+         self.selectPrevious();
+         self.chooseSlide(self.addr, false);
+         break;
+      }
     }
   });
 
