fdfa3b6f1736132a8d8d26593be89f39ad3e256b
Ilter Canberk <ilteriscanberk@gmail.com>
2013-04-25 21:45:46 +0300

    fixed homepage

--- v1factory/static/js/app/pages/home-main.js
+++ v1factory/static/js/app/pages/home-main.js
@@ -1,4 +1,39 @@
-define([
+require.config({
+  paths: {
+    "jquery" : "../../libs/jquery/jquery",
+    "jquery-ui" : "../../libs/jquery-ui/jquery-ui",
+    "underscore" : "../../libs/underscore-amd/underscore",
+    "backbone" : "../../libs/backbone-amd/backbone",
+    "iui" : "../../libs/iui/iui",
+    "comp": "../../libs/iui/comp",
+    "bootstrap" : "../../libs/bootstrap/bootstrap",
+    "app" : "../",
+    "editor" : "./../editor",
+    "dicts" : "../../dicts",
+    "mixins" : "../../mixins",
+    "key" : "../../libs/keymaster/keymaster"
+  },
+
+  shim: {
+    "jquery-ui": {
+      exports: "$",
+      deps: ['jquery']
+    },
+    "underscore": {
+      exports: "_"
+    },
+    "backbone": {
+      exports: "Backbone",
+      deps: ["underscore", "jquery"]
+    },
+    "bootstrap" : {
+      deps: ["jquery"]
+    }
+  }
+
+});
+
+require([
   'app/views/SimpleModalView',
   'app/views/LoginModalView'
 ],
@@ -27,10 +62,11 @@ function(SimpleModalView, LoginModalView) {
       $('.IN-widget').children().first().children().first().trigger('click');
     });
 
+    console.log('hey');
     $('#login-btn').on('click', function() {
       new LoginModalView();
     });
   };
 
-  return HomeMain;
+  new HomeMain();
 });
