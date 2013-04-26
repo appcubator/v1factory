8f932c2ef0f96cf6974a64179f91e65813351927
Karan Sikka <kssworld93@gmail.com>
2013-04-25 23:19:03 -0700

    dns records

--- v1factory/domains.py
+++ v1factory/domains.py
@@ -5,24 +5,24 @@ class DomainAPI(object):
   """Does all domain registration and configuration"""
 
   def call_api(self, url_ending, data, method="get", test=False):
-    assert url_ending[0] == '/' # just trying to make sure it's kosher
+    assert url_ending[0] == "/" # just trying to make sure it"s kosher
     if test:
-      url = 'https://test.dnsimple.com' + url_ending
+      url = "https://test.dnsimple.com" + url_ending
     else:
-      url = 'https://dnsimple.com' + url_ending
+      url = "https://dnsimple.com" + url_ending
 
     # set up arguments for requests function
     args = [ url ]
     kwargs = {
       "data":data,
-      "headers": {'Accept': 'application/json' }
+      "headers": { "Accept": "application/json", "Content-Type":"application/json" }
     }
 
-    # if test, then don't use basic auth
+    # if test, then don"t use basic auth
     if test:
-      kwargs['auth'] = ('founders@v1factory.com', 'domains,mayne,test')
+      kwargs["auth"] = ("founders@v1factory.com", "domains,mayne,test")
     else:
-      kwargs['auth'] = ('founders@v1factory.com', 'domains,mayne')
+      kwargs["auth"] = ("founders@v1factory.com", "domains,mayne")
 
     # call the right method
     method = method.lower()
@@ -42,7 +42,7 @@ class DomainAPI(object):
     """True if available, False if unavailable,
                 Exception if invalid and explosion if API call fails. jk."""
     try:
-      status_code, response = self.call_api('/domains/%s/check' % domain, {})
+      status_code, response = self.call_api("/domains/%s/check" % domain, {})
     except requests.exceptions.RequestException:
       # I guess this is pretty bad... Maybe it should be logged
       raise Exception("Could not make API call to dnsimple")
@@ -50,12 +50,12 @@ class DomainAPI(object):
 
     if status_code == 200:
       response = simplejson.loads(response)
-      assert response['status'] != 'available'
+      assert response["status"] != "available"
       return False
 
     elif status_code == 404: # <= really messed up API. http://developer.dnsimple.com/domains/#check-domain-availability
       response = simplejson.loads(response)
-      assert response['status'] == 'available'
+      assert response["status"] == "available"
       return True
 
     elif status_code == 400:
@@ -74,16 +74,16 @@ class DomainAPI(object):
     if len(domain) < 4:
       raise Exception("Domain too short.")
 
-    valid_domains = ['.com', '.net', '.org']
+    valid_domains = [".com", ".net", ".org"]
     if domain[-4:] not in valid_domains:
       raise Exception("Domain not valid.")
 
     post_data = {"domain":{}}
-    post_data["domain"]['name'] = 'springtask.me'
-    post_data["domain"]['registrant_id'] = 14454
+    post_data["domain"]["name"] = "springtask.me"
+    post_data["domain"]["registrant_id"] = 14454
 
     try:
-      status_code, response = self.call_api('/domain_registrations', post_data, method="post", test=True) # wont charge the card
+      status_code, response = self.call_api("/domain_registrations", simplejson.dumps(post_data), method="post", test=True) # wont charge the card
     except requests.exceptions.RequestException:
       # I guess this is pretty bad... Maybe it should be logged
       raise Exception("Could not make API call to dnsimple")
@@ -95,3 +95,53 @@ class DomainAPI(object):
 
     else:
       raise Exception("Error: status code: %d, content: %s" % (status_code, response))
+
+
+  def configure_domain_records(self, domain, staging=True):
+    """Make an A record to the server, then make a CNAME for www to the normal thing"""
+    a_post_data = {
+        "record": {
+          "content": "54.241.24.176",
+          "record_type": "A",
+          "name": "",
+          "prio": 10,
+          "ttl": 60
+        }
+    }
+    cname_post_data = {
+        "record": {
+          "content": domain,
+          "record_type": "CNAME",
+          "name": "www",
+          "prio": 10,
+          "ttl": 60
+        }
+    }
+    if not staging:
+      assert False # TODO change the content of A record to production server IP addr
+
+    try:
+      status_code, response = self.call_api("/domains/%s/records" % domain, simplejson.dumps(a_post_data), method="post")
+    except requests.exceptions.RequestException:
+      raise Exception("Could not make API call to dnsimple")
+
+
+    # A request
+    if status_code == 201:
+      a_response = simplejson.loads(response)
+    else:
+      raise Exception("Error making A record: status code: %d, content: %s" % (status_code, response))
+
+    # CNAME request
+    try:
+      status_code, response = self.call_api("/domains/%s/records" % domain, simplejson.dumps(cname_post_data), method="post")
+    except requests.exceptions.RequestException:
+      raise Exception("Could not make API call to dnsimple")
+
+
+    if status_code == 201:
+      cname_response = simplejson.loads(response)
+      return (a_response, cname_response)
+    else:
+      raise Exception("Error making CNAME record: status code: %d, content: %s" % (status_code, response))
+
