905f601c551566a9eef92bfae68280570c3af19b
Ilter Canberk <ilteriscanberk@gmail.com>
2013-05-10 02:56:41 +0300

    Improved theme display view

--- v1factory/static/js/app/views/ThemeDisplayView.js
+++ v1factory/static/js/app/views/ThemeDisplayView.js
@@ -10,17 +10,21 @@ function() {
       'click #load-btn' : 'loadTheme'
     },
 
-    initialize: function(bone) {
-      _.bindAll(this, 'loadTheme');
+    initialize: function(data) {
+      _.bindAll(this, 'render', 'loadTheme');
 
-      this.id = bone.id;
-      this.theme = bone.theme;
-      this.title = bone.title;
+      console.log(data);
+
+      this.info  = data.themeInfo;
+      this.theme = data.theme;
       this.render();
     },
 
     render: function() {
-      this.el.innerHTML = '<div class="btn" id="load-btn">Load Theme</div>';
+      template = ['<h2 class="span30"><%= name %></h2>',
+                  '<div class="span10 hoff2"><img src="<%= image %>"></div>',
+                  '<div class="span16 offset2 hoff2 load"><div class="btn" id="load-btn">Load Theme</div></div>'].join('\n');
+      this.el.innerHTML = _.template(template, this.info);
     },
 
     loadTheme: function() {
@@ -32,7 +36,7 @@ function() {
         url: '/app/'+appId+'/uiestate/',
         data: JSON.stringify(uieState),
         success: function(data) {
-          $(self.el).append('Loaded.');
+          self.$el.find('.load').append('<div class="hoff2"><h3>Loaded!</h3></div>');
         }
       });
 
