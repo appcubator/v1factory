6593629e499e3fcf30a28b66aa07bca3b19dfed5
Karan Sikka <kssworld93@gmail.com>
2013-05-09 00:56:46 -0400

    build static in the fab file

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,13 @@ from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd, env, hosts
 from fabric.contrib.console import confirm
 
+
+@hosts('v1factory@appcubator.com')
+def build_static():
+  code_dir = '/var/www/v1factory/v1factory'
+  with cd(code_dir):
+    run("./static/build/build.sh")
+
 @hosts('v1factory@appcubator.com')
 def pull_prod():
   code_dir = '/var/www/v1factory'
