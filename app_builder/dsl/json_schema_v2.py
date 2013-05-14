d71d73d53e79fcca42269eb4038d9fa04f1ee777
Karan Sikka <kssworld93@gmail.com>
2013-05-14 19:01:21 -0400

    new schema idea

---
+++ app_builder/dsl/json_schema_v2.py
@@ -0,0 +1,119 @@
+from validator_v2 import DictInited
+
+# Users
+
+class EntityField(DictInited):
+    _schema = {
+        "name": { "_type" : "" },
+        "required": { "_type": False },
+        "type": { "_type" : "" }
+    }
+
+class UserConfig(DictInited):
+    _schema = {
+        "facebook": { "_type": True },
+        "linkedin": { "_type": True },
+        "twitter": { "_type": True },
+        "local": { "_type": True },
+        "fields": {
+            "_type": [],
+            "_each": { "_type": EntityField }
+        }
+    }
+
+
+# Entities
+class Entity(DictInited):
+    _schema = {
+        "name": { "_type" : "" },
+        "fields": { "_type": [], "_each": { "_type": EntityField }},
+    }
+
+
+# Pages
+class Layout(DictInited):
+    _schema = {
+        "width": { "_type": 0, "_min": 1, "_max": 64 },
+        "height": { "_type": 0, "_min": 1 },
+        "top": { "_type": 0, "_min": 0 },
+        "left": { "_type": 0, "_min": 0, "_max": 64 }
+    }
+
+
+class UIElement(DictInited):
+    _schema = {
+        "layout": { "_type": Layout },
+        "content": { "_type": "" },
+    }
+    # this is going to get a container_info key later on
+
+
+class Node(DictInited):
+    _schema = {
+        "layout": { "_type": Layout },
+        "content": { "_type": "" },
+    }
+
+
+class Form(DictInited):
+    _schema = {
+        "name": { "_type" : "" },
+        "action": { "_type":"" },
+        "fields": { "_type" : [], "_each": { "_type" : {}, "_mapping": {
+            "name": { "_type" : "" },
+            "placeholder": { "_type" : "" },
+            "label": { "_type" : "" },
+            "displayType": { "_type" : "" },
+            "type": { "_type" : "" },
+            "options": { "_type" : [], "_each": { "_type" : "" }}
+        }}},
+    }
+
+
+class FormContainer(DictInited):
+    _schema = {
+        "entity": { "_type": "" },
+        "action": { "_type": "" },
+        "form": { "_type" : Form }
+    }
+
+
+class Iterator(DictInited):
+    _schema = {
+        "entity": { "_type": "" },
+        "action": { "_type": "" },
+        "uielements": { "_type": [],"_each": { "_type": Node }}
+    }
+
+
+UIElement._schema['container_info'] = { "_one_of": [ {"_type": FormContainer},
+                                                     {"_type": Iterator},
+                                                     {"_type": None, "_null": True} ] }
+
+
+class Page(DictInited):
+    _schema = {
+        "name": { "_type" : "" },
+        "url": { "_type": {}, "_mapping": {
+            "urlparts": { "_type": [],
+                "_each": { "_type" : "" }
+            }
+        }},
+        "uielements": { "_type": [], "_each": { "_type": UIElement }},
+        "access_level": { "_type" : "" }
+    }
+
+
+# Put it all together, you get an App
+class App(DictInited):
+    _schema = {
+        "name": { "_type" : "", "_minlength": 2, "_maxlength": 255 },
+        "info": { "_type": {}, "_mapping":{
+            "domain": { "_type":"" },
+            "description": { "_type":"" },
+            "keywords": { "_type":"" },
+        }},
+        "users": { "_type": UserConfig },
+        "entities": { "_type": [], "_each": Entity },
+        "pages": { "_type": [], "_each": {"_type":Page} }
+    }
---
+++ app_builder/dsl/validator_v2.py
@@ -0,0 +1,106 @@
+""" Really cool JSON schema validator """
+
+
+def validate(thing, schema):
+  """Return a list of error messages. if there are no errors, the thing successfully validated, no problemo."""
+  errors = []
+
+  #  if the thing is null, see if it's allowed to be null, and if it is, let it pass
+  if thing is None:
+    try: assert('_null' in schema and schema['_null'])
+    except Exception: raise Exception("thing was null but wasn't supposed to be.\n\n\nschema:{}".format(repr(thing), schema))
+    else: return errors
+
+  if '_one_of' in schema:
+    for validation_schema in schema['_one_of']:
+      # try all the schemas until one works. if none work, throw an error and quit.
+      print "i'm in one_of"
+      new_errs = validate(thing, validation_schema)
+      if len(new_errs) == 0:
+        return errors # no point in extending new_errors since it's blank
+    # if you get to this point, none of the "one of" things were valid.
+    errors.append("None of the _one_of things matched.\n\n\nthing: {}\n\n\nschema:{}".format(repr(thing), schema))
+    return errors
+  assert '_one_of' not in schema
+
+  # make sure the type of the thing matches with the schema
+  try:
+    assert('_type' in schema)
+  except Exception:
+    raise Exception('schema structure doesn\'t begin with _type')
+
+  if type(thing) == type(""):
+    thing = unicode(thing)
+
+  try:
+    assert type(thing) == type(schema['_type']) or (type(thing) == type(u"") and type(schema['_type']) == type(""))
+  except Exception:
+    errors.append("type of this thing doesn't match schema.\n\n\nthing: {}\n\n\nschema:{}".format(repr(thing), schema))
+    return errors
+
+  if type(thing) == type([]):
+    try: assert('_each' in schema)
+    except Exception: raise Exception('found [] with no _each')
+    for minithing in thing:
+      errors.extend(validate(minithing, schema['_each']))
+
+  elif type(thing) == type({}):
+
+    try: assert('_mapping' in schema)
+    except Exception: raise Exception('found {} with no _mapping')
+
+    for key in schema['_mapping']:
+      try: assert(key in thing)
+      except Exception:
+        errors.append('found a key in the schema which is not part of thing. "{}", {}'.format(key, thing))
+      else:
+        errors.extend(validate(thing[key], schema['_mapping'][key]))
+
+  elif type(thing) == type("") or type(thing) == type(u""):
+    if "_minlength" in schema:
+      try: assert(len(thing) >= schema["_minlength"])
+      except Exception: errors.append('string length was less than minlength: \"{}\", minlength={}'.format(repr(thing), schema['_minlength']))
+    if "_maxlength" in schema:
+      try: assert(len(thing) <= schema["_maxlength"])
+      except Exception: errors.append('string length was greater than maxlength: \"{}\", maxlength={}'.format(repr(thing), schema['_maxlength']))
+
+  elif type(thing) == type(0):
+    if "_min" in schema:
+      try: assert(thing >= schema["_min"])
+      except Exception: errors.append('int was less than min: \"{}\", min={}'.format(repr(thing), schema['_min']))
+    if "_max" in schema:
+      try: assert(thing <= schema["_max"])
+      except Exception: errors.append('int was greater than max: \"{}\", max={}'.format(repr(thing), schema['_max']))
+
+  elif type(thing) == type(True):
+    pass
+
+  else:
+    raise Exception("type not recognized: {}".format(thing))
+
+  return errors
+
+from dsl.json_schema import APP_SCHEMA
+def validate_app_state(app_state):
+  assert 'Homepage' in [ p['name'] for p in app_state['pages'] ]
+  assert 'Registration Page' in [ p['name'] for p in app_state['pages'] ]
+  return validate(app_state, APP_SCHEMA)
+
+
+class DictInited(object):
+    _schema = {}
+
+    def __init__(self, data=None):
+        """Inits this object with the data passed"""
+        if data is not None:
+            assert type(data) == type({}), "Input to init must be a dict"
+            # turn dict into object
+
+    @classmethod
+    def create_from_dict(cls, d):
+        """
+        Validates the data,
+          then inits the object
+        """
+        validate(d, cls._schema)
+        cls(data=d)
