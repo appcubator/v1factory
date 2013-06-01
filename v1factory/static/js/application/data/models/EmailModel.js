68d835c817b3f962be699931f7a12d808bc8dfd2
Kedar Amladi <kamladi@gmail.com>
2013-05-31 22:10:22 -0700

    email page styling

--- v1factory/static/js/application/data/models/EmailModel.js
+++ v1factory/static/js/application/data/models/EmailModel.js
@@ -5,9 +5,9 @@ function() {
 
   var EmailModel = Backbone.Model.extend({
     defaults : {
-      "name"         : "default-email",
-      "subject" : "Default Subject",
-      "content"   : "You have received the default email."
+		"name"      : "New Email",
+		"subject"   : "New Subject",
+		"content"   : "Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?\n\nNormally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass."
     }
   });
 
