f5f7a6fa08b8c6d2f18f4a0bd0da09644001941d
Kedar Amladi <kamladi@gmail.com>
2013-06-03 12:21:22 -0700

    moved router code out of main.js to new file

--- v1factory/static/js/application/main-app/main.js
+++ v1factory/static/js/application/main-app/main.js
@@ -48,240 +48,24 @@ require.config({
 //libs
 require([
   "models/AppModel",
-  "mixins/SimpleModalView",
-  "mixins/ErrorModalView",
-  "tutorial/TutorialView",
-  "app/AppInfoView",
-  "app/EntitiesView",
-  "app/ThemesGalleryView",
-  "app/PagesView",
-  "app/OverviewPageView",
-  "editor/EditorView",
-  "mobile-editor/MobileEditorView",
-  "app/EmailsView",
+  "app/AppRouter",
   "app/RouteLogger",
   "editor/KeyDispatcher",
   "editor/MouseDispatcher",
-  "mixins/SimpleDialogueView",
-  "mixins/ErrorDialogueView",
   "backbone",
   "bootstrap",
   "iui",
   "comp"
 ],
 function (AppModel,
-          SimpleModalView,
-          ErrorModalView,
-          TutorialView,
-          InfoView,
-          EntitiesView,
-          ThemesGalleryView,
-          PagesView,
-          OverviewPageView,
-          EditorView,
-          MobileEditorView,
-          EmailsView,
+          AppRouter,
           RouteLogger,
           KeyDispatcher,
-          MouseDispatcher,
-          SimpleDialogueView,
-          ErrorDialogueView) {
-
-  var v1App = Backbone.Router.extend({
-
-    routes: {
-      "app/:appid/"          : "index",
-      "app/:appid/info/"     : "showInfoPage",
-      "app/:appid/entities/" : "showEntitiesPage",
-      "app/:appid/gallery/"  : "showThemesPage",
-      "app/:appid/pages/"    : "showPagesPage",
-      "app/:appid/editor/:pageid/" : "showEditor",
-      "app/:appid/mobile-editor/:pageid/" : "showMobileEditor",
-      "app/:appid/emails/"    : "showEmailsPage"
-    },
-
-    tutorialDirectory: [0],
-
-    initialize: function() {
-      var self = this;
-      $('#save').on('click', this.save);
-      $('#tutorial').on('click', this.showTutorial);
-    },
-
-    index: function () {
-      v1App.tutorialDirectory = [0];
-      this.changePage(OverviewPageView, {}, function() {
-        return;
-      });
-    },
-
-    showInfoPage: function() {
-      v1App.tutorialDirectory = [2];
-      this.changePage(InfoView, {}, function() {
-        $('.menu-app-info').addClass('active');
-      });
-    },
-
-    showEntitiesPage: function() {
-      v1App.tutorialDirectory = [3];
-      this.changePage(EntitiesView, {}, function() {
-        $('.menu-app-entities').addClass('active');
-      });
-    },
-
-    showThemesPage: function() {
-      v1App.tutorialDirectory = [4];
-      this.changePage(ThemesGalleryView, {}, function() {
-        $('.menu-app-themes').addClass('active');
-      });
-    },
-
-    showPagesPage: function() {
-      $('.page').fadeIn();
-      v1App.tutorialDirectory = [5];
-      this.changePage(PagesView, {}, function() {
-        $('.menu-app-pages').addClass('active');
-      });
-    },
-
-    showEditor: function(appId, pageId) {
-      $('.page').fadeOut();
-      v1App.tutorialDirectory = [5];
-
-      if(v1App.view) v1App.view.remove();
-      var cleanDiv = document.createElement('div');
-      cleanDiv.className = "clean-div editor-page";
-      $(document.body).append(cleanDiv);
-
-      v1App.view  = new EditorView({pageId: pageId});
-      v1App.view.setElement(cleanDiv).render();
-
-      olark('api.box.hide');
-      this.changeTitle(v1App.view.title);
-    },
-
-    showMobileEditor: function(appId, pageId) {
-      $('.page').fadeOut();
-      v1App.tutorialDirectory = [5];
-
-      if(v1App.view) v1App.view.remove();
-      var cleanDiv = document.createElement('div');
-      cleanDiv.className = "clean-div editor-page";
-      $(document.body).append(cleanDiv);
-
-      v1App.view  = new MobileEditorView({pageId: pageId});
-      v1App.view.setElement(cleanDiv).render();
-
-      olark('api.box.hide');
-      this.changeTitle(v1App.view.title);
-    },
-
-    showEmailsPage: function() {
-      v1App.tutorialDirectory = [6];
-      this.changePage(EmailsView, {}, function() {
-        $('.menu-app-emails').addClass('active');
-      });
-    },
-
-
-    changePage: function(newView, viewOptions, post_render) {
-      if(v1App.view) v1App.view.remove();
-      var cleanDiv = document.createElement('div');
-      cleanDiv.className = "clean-div";
-      $('#main-container').append(cleanDiv);
-      v1App.view = new newView(viewOptions);
-      v1App.view.setElement(cleanDiv).render();
-
-      $('.active').removeClass('active');
-      this.changeTitle(v1App.view.title);
-      post_render();
-    },
-
-    deploy: function() {
-      iui.startAjaxLoading();
-        $.ajax({
-              type: "POST",
-              url: '/app/'+appId+'/deploy/',
-              success: function(data) {
-                iui.stopAjaxLoading();
-                if(data.errors) {
-                  var content = { text: "There has been a problem. Please refresh your page. We're really sorry for the inconvenience and will be fixing it very soon." };
-                  if(DEBUG) {
-                    content = { text: data.errors };
-                  }
-                  new ErrorDialogueView(content);
-                }
-                else {
-                  new SimpleDialogueView({ text: 'Your app is available at <br /><a href="'+ data.site_url + '">'+ data.site_url +'</a>'});
-                }
-              },
-              error: function(data) {
-                var content = { text: "There has been a problem. Please refresh your page. We're really sorry for the inconvenience and will be fixing it very soon." };
-                if(DEBUG) {
-                  content = { text: data.responseText };
-                }
-                new ErrorModalView(content);
-              },
-              dataType: "JSON"
-        });
-    },
-
-    save: function() {
-      iui.startAjaxLoading();
-      appState = v1State.toJSON();
-      $.ajax({
-          type: "POST",
-          url: '/app/'+appId+'/state/',
-          data: JSON.stringify(appState),
-          complete: function() { iui.stopAjaxLoading("Saved"); },
-          error: function(data) {
-            if(data.responseText == "ok") return;
-            var content = { text: "There has been a problem. Please refresh your page. We're really sorry for the inconvenience and will be fixing it very soon." };
-            if(DEBUG) {
-              content = { text: data.responseText };
-            }
-            new ErrorModalView(content);
-          },
-          dataType: "JSON"
-      });
-    },
-
-    showTutorial: function(e, inp) {
-      if(!inp) inp = v1App.tutorialDirectory;
-      tutorial = new TutorialView(inp);
-    },
-
-    scrollUp: function() {
-      $('html,body').animate({scrollTop:0},100, "linear");
-    },
-
-    betaCheck: function(data) {
-      if(data.percentage > 30 && data.feedback === true) {
-        $('.notice').css('height', '118px');
-        $('.notice').html('<h3 class="">Thank you for joining Appcubator Private Beta program!</h3><div>You can claim your free domain from <a class="menu-app-info">Domain & SEO</a> page.</div>');
-        v1.menuBindings();
-      }
-
-      if(data.percentage > 30) {
-        $('#tutorial-check').prop('checked', true);
-      }
-      if(data.feedback === true) {
-        $('#feedback-check').prop('checked', true);
-      }
-    },
-
-    changeTitle: function(title) {
-      var newTitle = "";
-      if(title) {
-        newTitle = " | " + title;
-      }
-      document.title = "Appcubator" + newTitle;
-    }
-  });
+          MouseDispatcher) {
 
   v1State = new AppModel();
   v1State.initialize(appState);
-  v1 = new v1App();
+  v1 = new AppRouter();
 
   g_guides = {};
   keyDispatcher  = new KeyDispatcher();
