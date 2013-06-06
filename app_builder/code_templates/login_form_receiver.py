667327c56e3d1bd042d90e01f85bc23cc4fdb7fe
Karan Sikka <kssworld93@gmail.com>
2013-06-06 07:51:28 -0700

    forgot to add files

---
+++ app_builder/code_templates/login_form_receiver.py
@@ -0,0 +1,21 @@
+
+{% set JsonResponse = imports['django.JsonResponse'] %}
+{% set login_function = imports['django.auth.login'] %}
+{% set request = locals['request'] %}
+
+
+@require_POST
+def {{ fr.identifier }}({{request}}):
+    """
+    Handles the login action.
+    """
+    {#redirect_to = "{{ form_receiver.goto_view.view_path() }}"#}
+
+    form = {{ fr.form_id }}(None, data={{request}}.POST)
+    if form.is_valid():
+
+        {{login_function}}({{request}}, form.get_user())
+        return {{JsonResponse}}({}) #ajax_redirect(request, redirect_to)
+
+    else:
+        return {{JsonResponse}}(form.errors)
---
+++ app_builder/code_templates/signup_form_receiver.py
@@ -0,0 +1,31 @@
+
+{% set JsonResponse = imports['django.JsonResponse'] %}
+{% set login_function = imports['django.auth.login'] %}
+{% set authenticate_function = imports['django.auth.authenticate'] %}
+{% set request = locals['request'] %}
+
+@require_POST
+def {{ fr.identifier }}({{request}}):
+    """Create a User object"""
+    uform = {{fr.form_id}}({{request}}.POST)
+    {#pform = {{fr.user_profile_form_id}}({{request}}.POST)#}
+    if uform.is_valid():
+        {#} and pform.is_valid():#}
+        user = uform.save()
+        {#
+        profile = pform.save(commit=False)
+        profile.user = user
+        profile.save()#}
+
+        new_user = {{authenticate_function}}(username={{request}}.POST['username'],
+                                                        password={{request}}.POST['password1'])
+        {{login_function}}({{request}}, new_user)
+        return {{JsonResponse}}({})
+        {#ajax_redirect(request, "{{ form_receiver.goto_view.view_path() }}")
+        #}
+    else:
+        errors = {}
+        errors.update(uform.errors)
+        {#errors.update(pform.errors)
+        #}
+        return {{JsonResponse}}(simplejson.dumps(errors), mimetype="application/json")
