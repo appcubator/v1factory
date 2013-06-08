ee2ca577c068d7cb54137425c0ddcc6fe9b3d0b8
Karan Sikka <kssworld93@gmail.com>
2013-06-07 17:07:31 -0700

    now the ajaxify errors are showing up

--- app_builder/code_boilerplate/ajaxify.js
+++ app_builder/code_boilerplate/ajaxify.js
@@ -14,25 +14,27 @@ $(document).ready(function() {
       });
   }
 
-  $('form').each(function(ind, node) {
+  $('form').each(function(ind, this_form) {
 
-    $(node).submit(function(e) {
+    $(this_form).submit(function(e) {
       var self = this;
       var ajax_info = {
-        type : $(node).attr('method'),
-        url  : $(node).attr('action'),
-        data : $(node).serialize(),
+        type : $(this_form).attr('method'),
+        url  : $(this_form).attr('action'),
+        data : $(this_form).serialize(),
         success : function(data, statusStr, xhr) {
           if (typeof(data.redirect_to) !== 'undefined') {
             location.href = data.redirect_to;
           } else {
-            _.each(data, function(val, key, ind) {
-              if(key==='__all__') {
-                $(self).find('.form-error.field-all').html(val.join('<br />'));
-              } else {
-                $(self).find('.form-error.field-name-'+key).html(val.join('<br />'));
-              }
-            });
+            if (!data.success){
+              _.each(data.errors, function(val, key, ind) {
+                if(key==='__all__') {
+                  $(self).find('.form-error.field-all').html(val.join('<br />'));
+                } else {
+                  $(self).find('.form-error.field-name-'+key).html(val.join('<br />'));
+                }
+              });
+            }
           }
         }
       };
