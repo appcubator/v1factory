8b6f9fd013eb7e880a5cd665fc1dbf2645db9f5e
Karan Sikka <kssworld93@gmail.com>
2013-06-09 01:35:49 -0700

    BEAUTIFUL REFACTOR

---
+++ app_builder/codes/forms.py
@@ -0,0 +1,35 @@
+from app_builder import naming
+
+from . import env
+
+
+class DjangoForm(object):
+
+    def __init__(self, identifier, model_id, field_ids):
+        """
+        For now it'll only work with model fields
+        """
+        self.identifier = identifier
+        self.namespace = naming.Namespace(parent_namespace=identifier.ns)
+        self.model_id = model_id
+        self.code_path = 'webapp/forms.py'
+        self.field_ids = field_ids
+
+    def render(self):
+        if len(self.field_ids) == 1:
+            self.included_field_string = repr(str(self.field_ids[0])) + ','
+        else:
+            self.included_field_string = ', '.join([repr(str(i)) for i in self.field_ids])
+        return env.get_template('form.py').render(form=self, imports=self.namespace.imports(), locals={})
+
+
+# HACK TO BE REFACTORED LATER
+class DjangoLoginForm(DjangoForm):
+
+    def __init__(self, identifier):
+        self.identifier = identifier
+        self.code_path = 'webapp/forms.py'
+
+    def render(self):
+        return ""
+
---
+++ app_builder/codes/models.py
@@ -0,0 +1,130 @@
+from app_builder import naming
+
+from . import env
+
+
+class DjangoQuery(object):
+
+    def __init__(self, model_id):
+        self.model_id = model_id
+
+    def render(self):
+        return "%s.objects.all()" % self.model_id
+
+
+class DjangoField(object):
+
+    _type_map = {'text': 'TextField',
+                 'number': 'FloatField',
+                 'date': 'DateTimeField',
+                 '_CREATED': 'DateTimeField',
+                 '_MODIFIED': 'DateTimeField',
+                 'email': 'EmailField',
+                 'image': 'TextField',
+
+                 }
+
+    def __init__(self, identifier, canonical_type, required=False, parent_model=None):
+        """parent_model is a DjangoModel instance"""
+        self.identifier = identifier
+        self.canon_type = canonical_type
+        self.django_type = self.__class__._type_map[canonical_type]
+        self.required = required
+        self.model = parent_model
+        self.args = []
+
+    def kwargs(field):
+        kwargs = {}
+        if field.canon_type == '_CREATED':
+            kwargs['auto_now_add'] = True
+        elif field.canon_type == '_MODIFIED':
+            kwargs['auto_now'] = True
+        if field.required:
+            if field.canon_type in ['text', 'email', 'image']:
+                kwargs['default'] = repr("")
+            if field.canon_type in ['float', 'date']:
+                kwargs['default'] = 0
+        else:
+            kwargs['blank'] = True
+            if field.django_type not in ('TextField', 'CharField'):
+                kwargs['null'] = True
+        return kwargs
+
+class DjangoRelatedField(object):
+    """Should abide by the same interface as DjangoField"""
+    _type_map = {
+        'fk': 'ForeignKey',
+        'm2m': 'ManyToManyField',
+        'o2o': 'OneToOneField',
+    }
+
+    def __init__(self, identifier, relation_type, rel_model_id, rel_name_id, required=True, parent_model=None):
+        self.identifier = identifier
+        self.rel_type = relation_type
+        self.django_type = self.__class__._type_map[relation_type]
+        self.required = required
+        self.model = parent_model
+        self.rel_model_id = rel_model_id
+        self.rel_name_id = rel_name_id
+        self.args = [repr(str(rel_model_id))]
+
+    def kwargs(field):
+        kwargs = {}
+        kwargs['related_name'] = repr(str(field.rel_name_id))
+        if not field.required:
+            kwargs['blank'] = repr(True)
+            kwargs['null'] = repr(True)
+        return kwargs
+
+
+class DjangoModel(object):
+
+    def __init__(self, identifier):
+        self.identifier = identifier
+        identifier.ref = self
+        self.code_path = "webapp/models.py"
+        self.namespace = naming.Namespace(parent_namespace=self.identifier.ns)
+        self.fields = []
+
+    def create_field(self, name, canonical_type, required):
+        identifier = self.namespace.new_identifier(name)
+        f = DjangoField(
+            identifier, canonical_type, required=required, parent_model=self)
+        self.fields.append(f)
+        return f
+
+    def create_relational_field(self, name, relation_type, rel_model_id, rel_name_id, required):
+        assert relation_type in ['o2o', 'm2m', 'fk']
+        identifier = self.namespace.new_identifier(name)
+        f = DjangoRelatedField(
+            identifier, relation_type, rel_model_id, rel_name_id, required=required, parent_model=self)
+        self.fields.append(f)
+        return f
+
+    def create_query(self): # will add more options for query later
+        q = DjangoQuery(self.identifier)
+        return q
+
+    def render(self):
+        return env.get_template('model.py').render(model=self, imports=self.namespace.imports(), locals={})
+
+
+class DjangoUserModel(DjangoModel):
+
+    def __init__(self, user_identifier, user_prof_identifier):
+        """Provide:
+        1. the identifier for the user (imported from django.contrib.models...),
+        1. the identifier for the userprofile class (imported from django.contrib.models...),
+
+        """
+        super(DjangoUserModel, self).__init__(user_identifier)
+        self.user_profile_identifier = user_prof_identifier
+        self.namespace = user_prof_identifier.ns
+        self.locals = {}
+        self.locals['user o2o'] = self.namespace.new_identifier('user')
+
+    def create_query(self):
+        raise Exception("what up brah. this is not yet implemented")
+
+    def render(self):
+        return env.get_template('usermodel.py').render(model=self, imports=self.namespace.imports(), locals=self.locals)
